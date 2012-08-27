4ba242b031afd2fc476fb83207f5a65c328c9a60
Alex Myers <alex@ajm-netbook.(none)>
2012-08-27 19:23:03 -0400

    Added Hello World Program
    This commit adds a basic HelloWorld Program.

---
+++ Hello_World/hello_world.adb
@@ -0,0 +1,9 @@
+-- This program will take input of of 3 values and find both the sum and the product of each value.
+-- The original values will be output along with the answers
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Hello_World is
+begin 
+		Put("This is my 5th attempt at Ada 2005");
+		New_Line;
+		Put("Alex Myers");
+end Hello_World;
